247e09859720c0f56483ad3d7f5ba9132ccbbcac
Spencer <spencerfinch7@gmail.com>
2019-12-22 12:43:41 -0500

    added a payday 2 wav to stream converter Signed-off-by: Spencer <spencerfinch7@gmail.com>

---
+++ wav_to_stream.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#converts all wav files in directory to stream files, used for payday 2 (and probably other games)
+#uses the payday 2 sound converter:
+#https://bitbucket.org/zabb65/payday-2-modding-information/downloads/sound_conveter_v1.15.zip
+#windows executable only
+#...yeah im using a linux subsystem for windows, maybe it'd work in WINE tho (or a VM)
+
+for f in "*.wav"
+do 
+	./wwise_ima_adpcm.exe -e "$f" "$f"
+done
